8db3aaf778c7a482ecffca446e07e0aa4a29152a
F.J. Valverde <fva@tsc.uc3m.es>
2015-11-21 17:44:52 +0100

    Allowed entropies.table to process more than 3 dimensions. Dimensions > 2 get propagated to the output df.

--- R/entropies.R
+++ R/entropies.R
@@ -23,8 +23,10 @@ entropies.table <- function(Nxy, ...){
     # 0. Parameter checking
     Nxy <- as.table(Nxy) # is this necessary?
     dims <- dim(Nxy)
-    if (length(dims) < 2 | length(dims) > 3)
-        stop("Cannot process tables with more than 3 dimensions or less than 2 dimensions.")
+    if (length(dims) < 2)
+        stop("Cannot process tables with less than 2 dimensions.")
+#    if (length(dims) < 2 | length(dims) > 3)
+#        stop("Cannot process tables with more than 3 dimensions or less than 2 dimensions.")
     if (dims[1] < 2 | dims[2] < 2)
         stop("Entropies are not defined for distributions with a singleton domain.")
     # 1. Start processing: this is a candidate por entropies_raw
@@ -53,7 +55,7 @@ entropies.table <- function(Nxy, ...){
         Uy <- apply(Ny, 2, function(x) { log2(length(x))})
         Hxy <- apply(Nxy, 3, function(x) {do.call(entropy, c(list(y=x), vars))})
         df <- data.frame(Ux = Ux, Uy = Uy, Hx = Hx, Hy = Hy, Hxy = Hxy)
-        df <- cbind(df, dimnames(Nxy)[3])# Keep the third dimension's name
+        df <- cbind(df, dimnames(Nxy)[3:])# Keep the third  and greater dimension's names
         #name <- colnames(N[1,,]) # This is a hack to manifest the values in the 3rd dimension
     } 
     return(df)
